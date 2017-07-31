a43cc4ef281000eca2d1edfdef14be1eded99620
Vincent Moscatello <moscatello.vincent@gmail.com>
2017-07-31 18:52:52 -0400

    Created framebuffer object

--- rotating_cube.c
+++ rotating_cube.c
@@ -13,11 +13,20 @@
 #define CYELL "\x1B[33m"
 #define CCYAN "\x1B[36m"
 
+
+struct framebuffer_object{
+    int fd;
+    uint8_t * fbp;
+    struct fb_fix_screeninfo fsi;
+    struct fb_var_screeninfo vsi;
+};
+
+
 static void print_fix_si(struct fb_fix_screeninfo * si){
     fprintf(stderr, CYELL"====> FIXED SCREENINFO\n"CNONE);
     fprintf(stderr, "id:        %s\n",  si->id);
-    fprintf(stderr, "smem_s:    %lu\n", (unsigned long) si->smem_start);
-    fprintf(stderr, "smem_len:  %lu\n", (unsigned long) si->smem_len);
+    fprintf(stderr, "smem_s:    %lx\n", (unsigned long) si->smem_start);
+    fprintf(stderr, "smem_len:  %lx\n", (unsigned long) si->smem_len);
     fprintf(stderr, "type:      %lu\n", (unsigned long) si->type);
     fprintf(stderr, "type_aux:  %lu\n", (unsigned long) si->type_aux);
     fprintf(stderr, "visual:    %lu\n", (unsigned long) si->visual);
@@ -47,47 +56,61 @@ static void print_var_si(struct fb_var_screeninfo * si){
     fprintf(stderr, "accelflag: %lu\n", (unsigned long) si->accel_flags);
 }
 
-int main(){
-    
-    //start off by just printing the important frame buffer structs.
-    struct fb_fix_screeninfo fsi;
-    struct fb_var_screeninfo vsi;
-    int fd = open("/dev/fb0", O_RDWR);
 
-    if(fd == -1){
+static uint32_t * get_pixel(struct framebuffer_object * fbo, int x, int y){
+    long location = (x * fbo->vsi.bits_per_pixel) + (y * fbo->fsi.line_length);
+    return (uint32_t *) (fbo->fbp + location);
+}
+
+static int init_framebuffer(struct framebuffer_object * fbo){
+    fbo->fd = open("/dev/fb0", O_RDWR);
+
+    if(fbo->fd == -1){
         fprintf(stderr, "Failed to open /dev/fb0\n");
-        exit(1);
+        return -1;
     }
 
-    if(ioctl(fd, FBIOGET_FSCREENINFO, &fsi) == -1){
+    if(ioctl(fbo->fd, FBIOGET_FSCREENINFO, &fbo->fsi) == -1){
         fprintf(stderr, "Failed to get FSCREENINFO\n");
-        exit(1);
+        return -1;
     }
     
-    if(ioctl(fd, FBIOGET_VSCREENINFO, &vsi) == -1){
+    if(ioctl(fbo->fd, FBIOGET_VSCREENINFO, &fbo->vsi) == -1){
         fprintf(stderr, "Failed to get VSCREENINFO\n");
-        exit(1);
+        return -1;
     }
 
-    print_fix_si(&fsi);
-    print_var_si(&vsi);
+    //Set pointer to frame buffer dma
+    fbo->fbp = mmap( NULL, 
+                    fbo->fsi.smem_len, 
+                    PROT_READ | PROT_WRITE, 
+                    MAP_SHARED,
+                    fbo->fd,
+                    0);
     
-    puts(CCYAN"[PRESS KEY TO CONTINUE]"CNONE);
+    if(fbo->fbp == MAP_FAILED)
+        return -1;
+    
+    return 0;
+}
+
+
+
+int main(){
+    struct framebuffer_object fbo;
+    
+    if(init_framebuffer(&fbo) == -1){
+        fprintf(stderr, "Failed to create framebuffer object.\n");
+        exit(-1);
+    }
+
+    //start off by just printing the important frame buffer structs.
+
+    print_fix_si(&fbo.fsi);
+    print_var_si(&fbo.vsi);
+    
+    puts(CCYAN"[PRESS ENTER TO CONTINUE]"CNONE);
     getchar();
-    
-
-    /*
-     * Drawing to screen
-     */
-
-    //Get a pointer to frame buffer dma
-    uint8_t * fbp = mmap(   NULL, 
-                            fsi.smem_len, 
-                            PROT_READ | PROT_WRITE, 
-                            MAP_SHARED,
-                            fd,
-                            0                       );
-    
 
 }
 
